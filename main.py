3683eb5c36a4d200c13924e01df8482d7a1a6d98
drewwmercer <awmercer@gmail.com>
2021-01-25 21:45:45 -0500

    Setting up app with a single route

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask("mpg_estimator")
 
 @app.route('/', methods=['GET'])
 def ping():
-    return "Pinging Model Application"
+    return "Pinging Model Application - Successful"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
